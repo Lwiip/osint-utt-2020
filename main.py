d8507e7a58cb682a8e6a2db0325cc84d1b690657
“Lwiip” <louisgalava.net@gmail.com>
2021-01-17 15:55:25 +0100

    structure main ok

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import argparse
+import sys
 import virustotal
 
 
 ###################################################
-#options
+#options and inputs from user
 ###################################################
 parser = argparse.ArgumentParser()
-parser.add_argument("-vt", "--virustotal", help="", action="store_true")  
+parser.add_argument("-ip", help="Indicate the IP address to look for")   
+parser.add_argument("-url", help="Indicate the url to look for")   
+parser.add_argument("-vt", "--virustotal", help="virustotal check", action="store_true")  
 args = parser.parse_args()
 
-print(args.virustotal)
+#if there is no ip or url specified by the user
+if not args.ip and not args.url:
+    print("you must specify an ip or an url, please retry")
+    sys.exit()
 
+#Get inputs from user
+datauserip = {}
+if args.ip:
+    counter = 0
+    for ip in args.ip.split(','):
+        datauserip["ip_{0}".format(counter)] = ip
+        counter = counter +1
+
+datauserurl = {}
+if args.url:
+    counter = 0
+    for url in args.url.split(','):
+        datauserurl["url_{0}".format(counter)] = url
+        counter = counter +1
+
+###################################################
+#launch options
+###################################################
 if args.virustotal:
-    #function to do in virustotal
-    virustotal
+    virustotal.mainvt(datauserip, datauserurl)
+
+
+
 
 
 # option pour activer les modules. 
--- virustotal.py
+++ virustotal.py
@@ -1,26 +1,43 @@
-import vt
 
 #https://virustotal.github.io/vt-py/overview.html
 #https://developers.virustotal.com/v3.0/reference#overview
 
 
 
-# API config
-apikey = "2285799e398a78710bd7f2181cc971fbfca0dc59b6311f7b7f18924c87550df6"
-client = vt.Client(apikey)
+
+# Main vt
+def mainvt(datauserip, datauserurl):
+    """
+    Description of the function
+    :param datauser:dictionnary, ip and url from user 
+    """
+    # API config
+    import vt
+    apikey = "2285799e398a78710bd7f2181cc971fbfca0dc59b6311f7b7f18924c87550df6"
+    client = vt.Client(apikey)
+        
+    
+    #IP
+    if datauserip != {}:
+        for _, ip in datauserip.items():
+            vtip(vt, client, ip)
+    #URL
+    if datauserurl != {}:
+        for _, url in datauserurl.items():
+            vturl(vt, client, url)
+    
+
+    #Close API
+    client.close()
+
+    
+def vturl(vt, client, url):
+    url_id = vt.url_id(url)
+    url = client.get_object("/urls/{}".format(url_id))
+    print(url.last_analysis_stats)
 
 
+#def vtip(vt, ip):
 
 
-# example
-url_id = vt.url_id("http://www.virustotal.com")
-url = client.get_object("/urls/{}".format(url_id))
-
-
-#print
-print(url.last_analysis_stats)
-
-
-#Close API
-client.close()
 
